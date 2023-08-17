9b4c1844e170719c48ed1e67f3ef6190afd9e893
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-08-17 08:15:57 -0300

    Foi populado a tabela fornecedores com o método seeder

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+use Illuminate\Support\Facades\DB;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //instanciando o objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'Fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        //método create(atenção para o atributo fillable da classe Fornecedor em model)
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'Fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        //insert
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'Fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+    }
+}
