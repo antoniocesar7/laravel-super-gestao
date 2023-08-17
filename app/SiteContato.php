d3a8886b5b065a8fcc86b6c5f79efafcb5e7d7ef
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-08-17 08:45:37 -0300

    Populando com o seeder a tabela site_contatos

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SiteContato extends Model
 {
     //
+    /*use SoftDeletes;
+    protected $table = 'site_contatos';
+    protected $fillable = ['name','telefone','email','motivo_contato','mensagem'];*/
 }
