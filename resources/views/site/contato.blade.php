82f867b7fcc88b3d365a4a93d8c01c09c0f63009
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-08-21 09:30:32 -0300

    Alterado  o sected para recuperar informações do banco

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -12,14 +12,14 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    @component('site.layouts._components.form_contato',['classe' => 'borda-preta'])
+                    @component('site.layouts._components.form_contato',['classe' => 'borda-preta','motivo_contatos' => $motivo_contatos])
                     <p>A nossa equipe analisará a sua mensagem e retornaremos o mais breve possível!</p>
                     <p>Nosso tempo médio de resposta é em até em 48 horas</p>
                     @endcomponent
                 </div>
             </div>  
         </div>
-
+{{-- {{print_r($motivo_contatos)}} --}}
         <!-- <div class="rodape">
             <div class="redes-sociais">
                 <h2>Redes sociais</h2>
