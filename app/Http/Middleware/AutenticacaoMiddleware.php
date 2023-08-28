82c2a391118e106bd94a1532014c938bd5f66574
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-08-28 16:30:53 -0300

    Trabalhando com variáveis de sessão global

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -14,33 +14,12 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next,$metodo_autenticacao,$perfil,$parametro3,$parametro4)
-    {
-        //teste para verificar se está recebendo o $metodo_autenticação das rostas app
-        echo $metodo_autenticacao.' - '.$perfil.'<br>';
-
-        if($metodo_autenticacao == 'padrao'){
-            echo 'Verificar o usuário e senha no banco de dados'.$perfil.'<br>';
-        }
-
-        if($metodo_autenticacao == 'ldap'){
-            echo 'Verificar o usuário e senha no AD'.$perfil.'<br>';
-        }
-
-        if($perfil == 'visitante'){
-            echo 'Exibir apenas alguns recursos'.'<br>';
-        }else{
-            echo 'Carregar o perfil do banco de dados'.'<br>';
-        }
-
-        //teste para verificar se o usuário tem acesso a rota...
-        if(false){
+    public function handle($request, Closure $next,$metodo_autenticacao,$perfil,$parametro3,$parametro4){
+        session_start();
+        if(isset($_SESSION['email']) && $_SESSION['email'] != ''){
             return $next($request);
         }else{
-            return Response('Acesso negado! Rota exige autenticação!!!');
+            return redirect()->route('site.login',['erro' => 2]);
         }
-
-        //return $next($request);
-        
     }
 }
