91f6f896d7d383fbe601e463543f3b5ba9242fc2
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-09-19 16:44:43 -0300

    Utilizando a o opção de acessar eagerloadign(acesso mais rápido)

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -17,6 +17,7 @@
 
             <div class="informacao-pagina">
                 <div style="width: 90%; margin-left: auto; margin-right:auto;">
+                     {{-- {{$produtos->toJson()}} --}}
                     <table border="1" width="100%">
                         <thead>
                             <tr>
@@ -57,7 +58,7 @@
                             @endforeach
                         </tbody>
                     </table>
-
+                    {{-- {{$produtos->toJson()}} --}}
                     {{$produtos->appends($request)->links()}}
                     <br>
                     
