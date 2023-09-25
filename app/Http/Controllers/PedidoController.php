7df63ad1beb17ef993e6516c1dd485f62ac28e59
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-09-25 15:44:36 -0300

    Foi adicionado tela e trabalhado no método para inserir pedidos

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Cliente;
 use Illuminate\Http\Request;
 use App\Pedido;
 
@@ -27,6 +28,10 @@ class PedidoController extends Controller
     public function create()
     {
         //
+        $clientes = Cliente::all();
+        //dd($clientes);
+        return view('app.pedido.create',['clientes' => $clientes]);
+
     }
 
     /**
@@ -38,6 +43,21 @@ class PedidoController extends Controller
     public function store(Request $request)
     {
         //
+        $regras = [
+            'cliente_id' => 'exists:clientes,id'
+        ];
+
+        $feedback = [
+            'cliente_id.exists' => 'O cliente informado não exite'
+        ];
+
+        $request->validate($regras,$feedback);
+
+        $pedido = new Pedido();
+        $pedido->cliente_id = $request->get('cliente_id');
+        $pedido->save();
+
+        return redirect()->route('pedido.index');
     }
 
     /**
